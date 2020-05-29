1f9cd9075c662fa1312ef82b10df4ea8c798da8e
Ashvin-G <ashvingaonkar@gmail.com>
2020-05-29 23:42:37 +0530

    Capability to add text box

--- canvas.py
+++ canvas.py
@@ -5,6 +5,9 @@ drawing = False
 point1 = ()
 point2 = ()
 
+canvas_height = 977
+canvas_width = 1440
+
 def mouse_drawing(event, x, y, flags, params):
     global point1, point2, drawing
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -22,7 +25,7 @@ cv2.namedWindow("canvas")
 cv2.setMouseCallback("canvas", mouse_drawing)
 
 while True:
-    canvas = np.ones((460, 814, 3), dtype=np.uint8)
+    canvas = np.ones((canvas_height, canvas_width, 3), dtype=np.uint8)
     canvas = canvas * 255
 
     if point1 and point2:
@@ -35,3 +38,43 @@ while True:
         break
 
 cv2.destroyAllWindows()
+
+margin_top = str(int((point1[1]/460)*100)) + "px"
+margin_left = str(point1[0]) + "px"
+height = str(point2[1] - point1[1]) + "px"
+width = str(point2[0] - point1[0]) + "px"
+
+f = open("index.html", "w")
+f.write("<!DOCTYPE html>\n")
+f.write("<html>\n")
+f.write("<body>\n")
+f.write("<input type=\"text\" id=\"fname\" name=\"fname\" style=\"margin-top:"+margin_top+"; margin-left: "+margin_left+"; height: "+height+"; width: "+width+"\">\n")
+f.write("</body>\n")
+f.write("</html>")
+f.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
